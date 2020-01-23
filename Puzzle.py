9b8bb105e5b008416961c2e3a5a0a113c9f8d3c0
DPigeon <D_Pig@encs.concordia.ca>
2020-01-23 12:00:35 -0500

    #2: Fixing some typos

--- Puzzle.py
+++ Puzzle.py
@@ -6,14 +6,15 @@ inputPath = "input/input.txt"
 def PrintAll(size, max_d, max_l, values):
     for i in range(len(size)):
         print()
-        print("Puzzle #" + str(i) + " with max_d = " + str(max_d[i]) + " max_l = " + str(max_l[i]))
+        print("Puzzle #" + str(i) + " with max_d = " + str(max_d[i]) + " and max_l = " + str(max_l[i]) + ":")
         Board.Board(int(size[i]), int(values[i])) # Print every board
+        print()
 
 def main():
     # Initializing
     inputParser = InputParser.InputParser(inputPath)
 
-    # Lists
+    # Lists of all boards from the inputPath by column #
     size = inputParser.GetSizes()
     values = inputParser.GetValues()
     max_d = inputParser.GetMaxDepth()

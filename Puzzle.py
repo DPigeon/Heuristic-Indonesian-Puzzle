6673fffcc99bc9183bb18ac4ba0b056ad29f4002
DPigeon <D_Pig@encs.concordia.ca>
2020-01-23 11:43:06 -0500

    #2: Print All Boards + Info

--- Puzzle.py
+++ Puzzle.py
@@ -3,16 +3,26 @@ import Board
 
 inputPath = "input/input.txt"
 
+def PrintAll(size, max_d, max_l, values):
+    for i in range(len(size)):
+        print()
+        print("Puzzle #" + str(i) + " with max_d = " + str(max_d[i]) + " max_l = " + str(max_l[i]))
+        Board.Board(int(size[i]), int(values[i])) # Print every board
+
 def main():
     # 1. Program will take an input file .txt
     # 2. It will analyse with the DFS, BFS & A*
     # 3. It will output 6 files .txt
 
+    # Initializing
     inputParser = InputParser.InputParser(inputPath)
+
+    # Lists
     size = inputParser.GetSizes()
-    for i in range(len(size)):
-        size = int(inputParser.GetSizes()[i])
-        valueOfBoard1 = int(inputParser.GetValues()[i])
-        Board.Board(size, valueOfBoard1) # Print every board
+    values = inputParser.GetValues()
+    max_d = inputParser.GetMaxDepth()
+    max_l = inputParser.GetMaxSearchPath()
+    
+    PrintAll(size, max_d, max_l, values)
 
 main()

510ecfeb0de21e0d8cb8d8099437c502e13e1578
DPigeon <D_Pig@encs.concordia.ca>
2020-01-25 13:28:44 -0500

    Fixing a defect when reading inputs

--- Puzzle.py
+++ Puzzle.py
@@ -7,7 +7,7 @@ def PrintAll(size, max_d, max_l, values):
     for i in range(len(size)):
         print()
         print("Puzzle #" + str(i) + " with max_d = " + str(max_d[i]) + " and max_l = " + str(max_l[i]) + ":")
-        Board.Board(int(size[i]), int(values[i])) # Print every board
+        Board.Board(int(size[i]), values[i]) # Print every board
         print()
 
 def main():
@@ -19,11 +19,11 @@ def main():
     values = inputParser.GetValues()
     max_d = inputParser.GetMaxDepth()
     max_l = inputParser.GetMaxSearchPath()
-    
+
     PrintAll(size, max_d, max_l, values)
 
     input("Run All Algorithms By Pressing Enter ")
     
-    # Call algorithms here
+    # Call algorithms here (take all boards in the input order)
 
 main()

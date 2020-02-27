90ce3b8a0edccd098a86ede46084aa494e18ae11
Kevin Luu <kevinateur@hotmail.com>
2020-02-27 11:14:38 -0500

    Add replacement node

--- Astar.py
+++ Astar.py
@@ -5,32 +5,32 @@ import Board
 import Node
 import OutputParser
 
+
 # Priority Queue Sorted by h(n) --> BFS
 # Priority Queue Sorted by f(n) --> A*
 
-class Astar: # Using BFS in this class too as it is derived of A* with g = 0
+class Astar:  # Using BFS in this class too as it is derived of A* with g = 0
 
     output_parser = OutputParser.OutputParser()
     timeStart = 0
     timeEnd = 0
 
-    heuristic = None # The heuristic function
+    heuristic = None  # The heuristic function
     # 0 --> counting heuristic
     # 1 --> future ahead 1 heuristic
     # 2 --> heuristic #1 + #2
-    heuristicNum = 2 # Change this number to try different heuristics
+    heuristicNum = 2  # Change this number to try different heuristics
 
     def __init__(self):
-        self.open_list = [] # Nodes currently getting evaluated, as a priority queue
+        self.open_list = []  # Nodes currently getting evaluated, as a priority queue
         self.closed_list = []  # List of nodes that has been visited
-        heapq.heapify(self.open_list) # Using a priority queue as heap queue
+        heapq.heapify(self.open_list)  # Using a priority queue as heap queue
         if self.heuristicNum == 0:
             self.heuristic = self.generate_heuristic_based_on_counting
         if self.heuristicNum == 1:
             self.heuristic = self.generate_heuristic_based_on_future_ones
         if self.heuristicNum == 2:
             self.heuristic = self.generate_heuristic_based_on_counting_and_future_ones
-
 
     def Astar(self, iteration, board, size, max_length, name, stringName):
         length = 0
@@ -40,23 +40,24 @@ class Astar: # Using BFS in this class too as it is derived of A* with g = 0
         self.timeStart = time.time()
 
         # Root node, which has a depth of 0
-        fn = 0 + self.heuristic(board.board) # First f(n) has g = 0
+        fn = 0 + self.heuristic(board.board)  # First f(n) has g = 0
         root_node = Node.Node(None, board, 0, fn)
 
         # Put the root node into the priority queue
-        heapq.heappush(self.open_list, (root_node.f, root_node)) # Acts as pairs [<f(n), Node>]
+        heapq.heappush(self.open_list, (root_node.f, root_node))  # Acts as pairs [<f(n), Node>]
 
         # While the open list has a node...
         while self.open_list:
-            current_node = heapq.heappop(self.open_list) # We pop the smallest element to come according to f(n)
+            current_node = heapq.heappop(self.open_list)  # We pop the smallest element to come according to f(n)
             f = current_node[0]
-            g = 0 # For BFS
+            g = 0  # For BFS
             if name == "astar":
                 g = current_node[1].get_depth()
             h = self.heuristic(current_node[1].get_current_board().board)
 
-            self.output_parser.create_search_files(iteration, name, f, g, h, current_node[1].get_current_board().transform_2d_to_1d())
-            length = length + 1 # Increment the nodes searched
+            self.output_parser.create_search_files(iteration, name, f, g, h,
+                                                   current_node[1].get_current_board().transform_2d_to_1d())
+            length = length + 1  # Increment the nodes searched
 
             # We look if the node is the state goal
             if current_node[1].get_current_board().check_goal_state():
@@ -79,19 +80,42 @@ class Astar: # Using BFS in this class too as it is derived of A* with g = 0
             # Create nodes for each of the child nodes by generating the next moves
             possible_moves = current_node[1].get_current_board().generate_possible_moves(int(size))
             for i in range(len(possible_moves)):
-                children_to_append = Board.Board(int(size), current_node[1].get_current_board().prioritize_board(possible_moves)[i])
-                totalEstimate = self.heuristic(children_to_append.board) # For BFS
+                children_to_append = Board.Board(int(size),
+                                                 current_node[1].get_current_board().prioritize_board(possible_moves)[
+                                                     i])
+                totalEstimate = self.heuristic(children_to_append.board)  # For BFS
                 if name == "astar":
-                    totalEstimate = current_node[1].get_depth() + 1 + self.heuristic(children_to_append.board) # heuristic for A*
-                node_to_add = Node.Node(current_node[1], children_to_append, current_node[1].get_depth() + 1, totalEstimate)
+                    totalEstimate = current_node[1].get_depth() + 1 + self.heuristic(
+                        children_to_append.board)  # heuristic for A*
+                node_to_add = Node.Node(current_node[1], children_to_append, current_node[1].get_depth() + 1,
+                                        totalEstimate)
                 current_node[1].add_children(node_to_add)
 
-            # Generate a list of children to add by removing the one's already in the open and closed list
-            children_to_add = [x for x in current_node[1].get_children() if x not in self.open_list and self.closed_list]
+            children_to_add = current_node[1].get_children()
 
-            # We add the next childrens to be checked with their estimate
-            for i in range(len(children_to_add)):
-                heapq.heappush(self.open_list, (children_to_add[i].get_estimate(), children_to_add[i]))
+            for child in children_to_add:
+
+                if child not in self.open_list and child not in self.closed_list:  # If not in open/closed list
+                    heapq.heappush(self.open_list, (child.get_estimate(), child))
+
+                elif child in self.open_list and child not in self.closed_list:  # In the open_list, and not in the closed...
+
+                    # Get index of first child 0 O(n)
+                    index = self.open_list.index(child)
+                    node_to_replace = self.open_list[index]
+                    if node_to_replace.get_estimate() > child.get_estimate():
+                        self.open_list.remove(node_to_replace)  # Removes le
+                        heapq.heappush(self.open_list, (child.get_estimate(), child))
+
+                elif child not in self.open_list and child in self.closed_list:  # In the closed list, and not in the open list
+
+                    # Get index of first child 0 O(n)
+                    index = self.closed_list.index(child)
+                    node_to_replace = self.closed_list[index]
+
+                    if node_to_replace.get_estimate() > child.get_estimate():
+                        self.closed_list.remove(node_to_replace)  # Removes le
+                        heapq.heappush(self.open_list, (child.get_estimate(), child))
 
         print("Could not find a solution path for Puzzle #" + str(iteration) + " with " + stringName + ".\n")
         self.output_parser.create_solution_files(iteration, name, None, False)
@@ -110,7 +134,7 @@ class Astar: # Using BFS in this class too as it is derived of A* with g = 0
             for j in range(size):
                 total_number_of_ones = total_number_of_ones + self.change_surroundings(board, i, j, size)
         return total_number_of_ones
-    
+
     def generate_heuristic_based_on_counting_and_future_ones(self, board):
         # Adds both heuristic 1 and 2
         h1 = self.generate_heuristic_based_on_counting(board)
@@ -137,9 +161,11 @@ class Astar: # Using BFS in this class too as it is derived of A* with g = 0
 
     def back_track(self, goal_node):
         back_track_list = []
-        current_node = Node.Node(goal_node.get_parent(), goal_node.get_current_board(), goal_node.get_depth(), goal_node.get_estimate())
-        back_track_list.append(current_node) # We add the goal state
-        while current_node.get_parent() != None:
-            back_track_list.insert(0, current_node.get_parent()) # We add each parents at the the beginning by back tracking
-            current_node = current_node.get_parent() # We create the next one to iterate
+        current_node = Node.Node(goal_node.get_parent(), goal_node.get_current_board(), goal_node.get_depth(),
+                                 goal_node.get_estimate())
+        back_track_list.append(current_node)  # We add the goal state
+        while current_node.get_parent() is not None:
+            back_track_list.insert(0,
+                                   current_node.get_parent())  # We add each parents at the the beginning by back tracking
+            current_node = current_node.get_parent()  # We create the next one to iterate
         return back_track_list

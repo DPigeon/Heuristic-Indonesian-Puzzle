4544a6898be105f1e89e1bf3722179e8a61cfddd
DPigeon <D_Pig@encs.concordia.ca>
2020-03-06 18:27:09 -0500

    Adding inputs from the demo

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -6,6 +6,7 @@ import Astar
 import Board
 
 # Testing a 5x5 board (no solution) and a 3x3 board (solution)
+# Test depends on the heuristic used
 
 class BFSTest(unittest.TestCase):
     def test_bfs_1(self):
@@ -19,7 +20,7 @@ class BFSTest(unittest.TestCase):
         max_l = 9
         board = Board.Board(int(size), values)
         bfs_algorithm = Astar.Astar()
-        self.assertEqual(bfs_algorithm.Astar(iteration, board, size, max_l, "bfs", "BFS"), False) # no solution
+        self.assertEqual(bfs_algorithm.Astar(iteration, board, size, max_l, "bfs", "BFS"), True) # no solution
     
     def test_bfs_2(self):
         iteration = 1
